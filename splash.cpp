d04fdd5e7b85f780ce42ccd8a2d4443dee6b2b04
akram20030 <your.email@example.com>
2023-06-09 05:39:57 +0200

    first2 commit

---
+++ splash.cpp
@@ -0,0 +1,8 @@
+#include "splash.h"
+
+
+splash::splash(const Cairo::RefPtr<Cairo::Context> &cr) 
+{
+    
+
+}
---
+++ splash.h
@@ -0,0 +1,15 @@
+#include <gtkmm.h>
+#include <cairomm/context.h>
+#include <vector>
+#include <iostream>
+
+class splash 
+{
+public:
+  splash(const Cairo::RefPtr<Cairo::Context> &cr) ;
+  
+
+private:
+
+
+};
